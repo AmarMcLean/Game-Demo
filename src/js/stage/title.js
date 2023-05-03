2e9136d5290745da8423ab0e0419b83a522e9599
AmarMcLean <114834915+AmarMcLean@users.noreply.github.com>
2023-05-03 16:56:02 -0400

    Add files via upload

--- src/js/stage/title.js
+++ src/js/stage/title.js
@@ -2,11 +2,11 @@ import { Stage, event, Sprite, game, loader, input, state } from 'melonjs';
 
 class ButtonEntity extends Sprite {
     constructor(x, y) {
-        super(x, y, {image: "player_run01", framewidth: 178, frameheight: 140});
+        super(x, y, {image: "Background", framewidth: 178, frameheight: 140});
         
         this.anchorPoint.set(0,0);
 
-        input.registerPointerEvent("pointerdown", this, this.pointerDown.bind(this));
+        input.registerPointerEvent("pointerdown", game.viewport, this.pointerDown.bind(this));
     }
 
     pointerDown () {
